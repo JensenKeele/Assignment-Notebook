88e96551dbde6f43305b10b28d58316a00cfd847
jkeele <jkeele@bsd220.org>
2026-01-31 14:02:05 -0600

    customized

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -11,45 +11,50 @@ struct ContentView: View {
     @State private var assignmentList = AssignmentList()
     @State private var showingAddItemView = false
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(assignmentList.items) { item in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(item.course).font(.headline)
-                            Text(item.description)
+            NavigationView {
+                List {
+                    ForEach(assignmentList.items) { item in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.course).font(.headline)
+                                Text(item.description)
+                            }
+                            Spacer()
+                            Text(item.dueDate, style: .date)
                         }
-                        Spacer()
-                        Text(item.dueDate, style: .date)
+                        .listRowBackground(Color.black.opacity(0.4))
                     }
+                    .onMove(perform: { indices, newOffset in
+                        assignmentList.items.move(fromOffsets: indices, toOffset: newOffset)
+                    })
+                    .onDelete(perform: { indexSet in
+                        assignmentList.items.remove(atOffsets: indexSet)
+                    })
                 }
-                .onMove(perform: { indices, newOffset in
-                    assignmentList.items.move(fromOffsets: indices, toOffset: newOffset)
-                })
-                .onDelete(perform: { indexSet in
-                    assignmentList.items.remove(atOffsets: indexSet)
-                })
+                .scrollContentBackground(.hidden)
+                .listStyle(.plain)
+                .navigationBarTitle("Assignment Notebook")
+                .sheet(isPresented: $showingAddItemView) {
+                    AddAssignmentView()
+                        .environment(assignmentList)
+                }
+                .navigationBarItems(leading: EditButton(),
+                                    trailing: Button(action: {
+                    showingAddItemView = true
+                }, label: {
+                    Image(systemName: "plus")
+                }))
             }
-            .navigationBarTitle("Assignment Notebook")
-            .sheet(isPresented: $showingAddItemView) {
-                AddAssignmentView()
-                    .environment(assignmentList)
-            }
-            .navigationBarItems(leading: EditButton(),
-                                trailing: Button(action: {
-                showingAddItemView = true
-            }, label: {
-                Image(systemName: "plus")
-            }))
         }
     }
-}
+
 struct AssignmentItem: Identifiable, Codable {
     var id = UUID()
     var course = String()
     var description = String()
     var dueDate = Date()
 }
+
 #Preview {
     ContentView()
 }
