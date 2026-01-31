8f19c3ea1beb6e84f4c020f2060558d98a5c4a12
jkeele <jkeele@bsd220.org>
2026-01-31 11:46:26 -0600

    completed step 7

--- Assignment Notebook/AssignmentList.swift
+++ Assignment Notebook/AssignmentList.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+@Observable
+class AssignmentList {
+    var items =  [AssignmentItem(course: "Algebra",description: "linear Equation", dueDate: Date()),
+                  AssignmentItem(course: "History", description: "Paper", dueDate: Date()),
+                  AssignmentItem(course: "Biology", description: "Disect some frogs", dueDate: Date())]
+}
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
+}
--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,14 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var assignmentItems =
-    [AssignmentItem(course: "Algebra",description: "linear Equation", dueDate: Date()),
-     AssignmentItem(course: "History", description: "Paper", dueDate: Date()),
-     AssignmentItem(course: "Biology", description: "Disect some frogs", dueDate: Date())]
+    @State private var assignmentList = AssignmentList()
     var body: some View {
         NavigationView {
             List {
-                ForEach(assignmentItems) { item in
+                ForEach(assignmentList.items) { item in
                     HStack {
                         VStack(alignment: .leading) {
                             Text(item.course).font(.headline)
@@ -26,21 +23,15 @@ struct ContentView: View {
                     }
                 }
                 .onMove(perform: { indices, newOffset in
-                    assignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+                    assignmentList.items.move(fromOffsets: indices, toOffset: newOffset)
                 })
                 .onDelete(perform: { indexSet in
-                    assignmentItems.remove(atOffsets: indexSet)
+                    assignmentList.items.remove(atOffsets: indexSet)
                 })
             }
             .navigationBarTitle("Assignment Notebook")
             .navigationBarItems(leading: EditButton())
         }
-    }
-    struct AssignmentItem: Identifiable {
-        var id = UUID()
-        var course = String()
-        var description = String()
-        var dueDate = Date()
     }
 }
 
